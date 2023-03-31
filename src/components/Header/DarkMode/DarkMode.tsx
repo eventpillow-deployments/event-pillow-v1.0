dae7ebe2735dcc5443575417e05af7a28b0b1fde
hashkazi00 <sahilkazi01@hotmail.com>
2023-03-31 22:03:38 +0530

    dynamic element return fixed

--- src/components/Header/DarkMode/DarkMode.tsx
+++ src/components/Header/DarkMode/DarkMode.tsx
@@ -16,6 +16,8 @@ const DarkMode = () => {
             <svg onClick={toggleTheme} xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-zinc-600 cursor-pointer hover:animate-moon" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
         )
     }
+    
+    return null;
 };
 
 export default DarkMode;
