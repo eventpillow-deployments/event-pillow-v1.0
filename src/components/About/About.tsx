3bf502e21f8cd525a4da26588fbbac2af730c33a
hashkazi00 <sahilkazi01@hotmail.com>
2023-04-14 22:23:21 +0530

    final fixes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -34,7 +34,7 @@ const About = () => {
                             you to get hired or hire a photographer, manage your tasks or events, be clear about your availability,
                             build a personal brand, manage payments and everything a photograher or studio will ever need.
                         </p>
-                        <p className="mb-4 text-xl font-semibold text-gray-500 dark:text-gray-400">So that you can Focus on doing your Best work, become Productive and Earn More.</p>
+                        <p className="mb-4 text-xl font-semibold text-gray-500 dark:text-gray-400">So that you can Focus on doing your Best work, become Productive and Earn more.</p>
                         <div className="mb-5">
                             <p className="mb-2 text-lg font-semibold text-sky-500">Our target is to help you:</p>
                             <p className="mb-1">- Become productive and make more money.</p>
@@ -60,11 +60,11 @@ const About = () => {
                     <img className="sm:row-span-2" src="https://ik.imagekit.io/cqught041/EP-Main-Website/AboutUS/sahil2.jpeg?updatedAt=1681487289994" alt="founders picture" />
                     <div className="flex flex-col gap-y-3 mt-4">
                         <Link className="flex gap-x-2 dark:text-slate-300 text-slate-900" href="https://www.linkedin.com/in/sahilkazi01/" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
                             <p>Sahil (Co-founder)</p>
                         </Link>
                         <Link className="flex gap-x-2 dark:text-slate-300 text-slate-900" href="https://www.linkedin.com/in/sahilkazi01/" target="_blank">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" stroke-linecap="round" stroke-linejoin="round" className="feather feather-youtube"><path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"/><polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.25" stroke-linecap="round" stroke-linejoin="round" className="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect x="2" y="9" width="4" height="12"/><circle cx="4" cy="4" r="2"/></svg>                           
                             <p>@SahilKazi</p>
                         </Link>
                         <Link className="flex gap-x-2 items-center dark:text-slate-300 text-slate-900" href="tel:+919322002763" target="_blank">
@@ -74,7 +74,17 @@ const About = () => {
                     </div>
                 </div>
                 <div className="">
-                    <p className="text-base mb-6 sm:mb-8 sm:w-10/12 dark:text-slate-300 text-slate-900">Having found WolveElectronics previously and worked at multiple startups as a software developer. I bring the vision of <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">EventPillow</span> to life. Taking care of everything from Tech to Growth.</p>
+                    <p className="text-base mb-6 sm:mb-8 sm:w-10/12 dark:text-slate-300 text-slate-900">Having found WolveElectronics previously and worked at multiple startups as a software developer. I bring the vision of <span className="text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-blue-600">EventPillow</span> to life. Taking care of everything from Tech to Growth. </p> 
+                    <p className="text-base mb-6 sm:mb-8 sm:w-10/12 dark:text-slate-300 text-slate-900">After having a exit from WolveElectronics this is my second try at building something that I would love to see being used and make the lives of freelance photographers and studios a little easier.</p>
+                    <p className="mb-4 text-lgf font-semibold text-sky-500">I Invite you to become a part of Event Pillow.</p>
+                    <div className="mt-6 sm:mt-auto flex items-center justify-between gap-x-6">
+                        <Link
+                            href="/#join-waitlist"
+                            className="rounded-md bg-sky-500 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer bg-gradient-to-r from-cyan-400 to-blue-600 via-purple-500 hover:from-blue-600 hover:to-cyan-400 animate-gradient-x"
+                        >
+                            Join the Waitlist today →
+                        </Link>
+                    </div>
                 </div>
             </div>
         </section>
