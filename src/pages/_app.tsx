88747870c8d0374f3c75fc78333a8842f3230bca
hashkazi00 <sahilkazi01@hotmail.com>
2023-05-15 17:22:49 +0530

    vercel analytics installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/globals.css'
 import Script from 'next/script'
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider>
       <Script src="https://getlaunchlist.com/js/widget-diy.js" defer/>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
