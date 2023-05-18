72aedf3126500aad79852e8add09ccc7732a82a6
Hamdrive <hamza.husein017@gmail.com>
2023-05-18 13:15:25 +0530

    [update] add protected routes on clientside

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,41 @@ import { Analytics } from '@vercel/analytics/react';
 
 import '@/styles/globals.css';
 import Script from 'next/script';
-import { ClerkProvider } from '@clerk/nextjs';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from '@clerk/nextjs';
 import Header from '@/components/Header/Header';
+import { useRouter } from 'next/router';
 
+const protectedRoutes = ['/settings', '/builder'];
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const { pathname } = router;
+  const isProtectedRoute = protectedRoutes.includes(pathname);
+
   return (
     <ClerkProvider>
       <ThemeProvider>
         <Script src="https://getlaunchlist.com/js/widget-diy.js" defer />
         <>
           <Header />
-          <Component {...pageProps} />
+          {isProtectedRoute ? (
+            <>
+              <SignedIn>
+                <Component {...pageProps} />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          ) : (
+            <>
+              <Component {...pageProps} />
+            </>
+          )}
         </>
         <Analytics />
       </ThemeProvider>
